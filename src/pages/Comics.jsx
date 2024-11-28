0fbdd134802b3fdeb66fd34e782589d66bb56be4
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-11-28 16:48:13 +0100

    added comics page with Home page model

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,7 +1,110 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Comics = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [skip, setSkip] = useState(0);
+  const [titleFilter, setTitleFilter] = useState("");
+  try {
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await axios.get(
+          `https://site--marvel-backend--cszclskmpcqr.code.run/comics?skip=${skip}&title=${titleFilter}`
+        );
+        // console.log(response.data);
+
+        setIsLoading(false);
+        setData(response.data);
+        console.log(response.data);
+      };
+      fetchData();
+    }, [skip, titleFilter]);
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <>
-      <div>Page comics</div>
+      {isLoading ? (
+        <div> Acquisition des données...</div>
+      ) : (
+        <div className="home">
+          <section className="sectionCharacters">
+            <div className="principalTitleCharacters">
+              <div className="buttonPageDiv">
+                {" "}
+                <button
+                  className={
+                    skip === 0 ? "offButtonPagePrecedente" : "buttonPage"
+                  }
+                  onClick={() => {
+                    setSkip(skip - 100);
+                  }}
+                >
+                  Page précedente
+                </button>
+                <button
+                  className={
+                    skip === data.count - 100
+                      ? "offButtonPageSuivante"
+                      : "buttonPage"
+                  }
+                  onClick={() => {
+                    setSkip(skip + 100);
+                    {
+                      skip === 47300 && setSkip(skip + 97);
+                    }
+                  }}
+                >
+                  Page suivante
+                </button>
+              </div>
+              <p>Comics Marvel</p>
+              <input
+                type="text"
+                placeholder="Rechercher un Comic"
+                className="searchBarCharacter"
+                onChange={(event) => {
+                  setTitleFilter(event.target.value);
+                }}
+              />
+            </div>
+            <div className="charAndImg">
+              {data.results.map((elem) => {
+                return (
+                  <>
+                    <div className="charAndDesc">
+                      <div className="linkComicsOfChar">
+                        <img
+                          className="imgChar"
+                          src={`${elem.thumbnail.path}.${elem.thumbnail.extension}`}
+                          alt=""
+                        />
+                        <p className="nameChar">{elem.title}</p>
+                      </div>
+                      {elem.description && (
+                        <button
+                          className="buttonDescCharacters"
+                          onClick={() => {
+                            // setVisible(!visible);
+                            // setDescription(elem.description);
+                          }}
+                        >
+                          Description
+                        </button>
+                      )}
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </section>
+          {/* {visible && (
+            <Modal setVisible={setVisible} description={description} />
+          )} */}
+        </div>
+      )}
     </>
   );
 };
